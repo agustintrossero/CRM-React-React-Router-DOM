d33da8612187be2c1e04230d0ca5a3cf48bdcd88
agustintrossero <agustintrossero@gmail.com>
2023-02-13 20:32:21 +0100

    graccias a react-router-dom hacemos uso de Layout, Outlet y Children para pasar informacione entre paginas sin tener q reutilizar codigo en las distintas rutas

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,25 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
+import Layout from './components/Layout'
+import NuevoCliente from './pages/NuevoCliente'
+import Index from './pages/Index'
+
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>INICIO</h1>
-  },
-  {
-    path: "/Nosotros",
-    element: <h1>Nosotros</h1>
+    element: <Layout/>,
+    children: [
+      {
+        index: true,
+        element: <Index/>
+      },
+      {
+        path: "/clientes/nuevo",
+        element: <NuevoCliente/>
+      }
+    ]
   }
-
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
---
+++ src/pages/Index.jsx
@@ -0,0 +1,10 @@
+
+function Index() {
+  return (
+    <div>
+      Cliente
+    </div>
+  )
+}
+
+export default Index
