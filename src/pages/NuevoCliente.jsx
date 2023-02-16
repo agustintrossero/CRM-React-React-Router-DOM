a9fa073600e7961015457e827595dd16f4021f05
agustintrossero <agustintrossero@gmail.com>
2023-02-16 19:59:47 +0100

    aplicando useActionData para pasarle la funcion creada al Form evaluando si hay errores e imprimiendo el nuevo componente Error.jsx con su children, el error con los estilos, proximo paso validar el email

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,6 @@
-import {useNavigate, Form} from 'react-router-dom'
+import {useNavigate, Form, useActionData} from 'react-router-dom'
 import Formulario from '../components/Formulario'
+import Error from '../components/Error'
 
 export async function action ({request}){
    //console.log("Submit al formulario...")
@@ -7,13 +8,27 @@ export async function action ({request}){
    const formData = await request.formData()
    //console.log(formData.get("nombre"))
    const datos = Object.fromEntries(formData)
-   console.log(datos)
+   //console.log(datos)
+
+    const errores = []
+   if(Object.values(datos).includes("")){
+    errores.push("Todos los campos son obligatorios")
+   }
+
+   if(Object.keys(errores).length){
+    //console.log("Hay errores ...")
+    return errores
+   }
+   //console.log(errores)
    return null
 }
 
 function NuevoCliente() {
 
+  const errores = useActionData()
   const navigate = useNavigate()
+
+  //console.log(errores)
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Clientes</h1>
@@ -28,13 +43,16 @@ function NuevoCliente() {
       </div>
       <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
         
+        {errores?.length && errores.map((error, i)=>
+          <Error key={i}>{error}</Error>
+        )}
         <Form
           method='POST'
         >
           <Formulario/>
             <input 
               type="submit" 
-              className='mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg'
+              className='mt-5 w-full bg-blue-800 hover:bg-blue-900 p-3 uppercase font-bold text-white text-lg cursor-pointer'
               value="Registrar Cliente"
               />
         </Form>
