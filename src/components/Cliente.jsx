74758e57ddabf008ba4be71ca1e638e78b80ace9
agustintrossero <agustintrossero@gmail.com>
2023-03-06 16:09:13 +0100

    accediendo mediante rutas dinamicas a el cliente que se quiere editar segun su ID, siguiente paso es verificar que el cliente exista segun ese ID y sino mostrar un error

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,9 +1,10 @@
-
+import { useNavigate } from "react-router-dom"
 
 function Cliente({cliente}) {
 
-
+    const navigate = useNavigate()
     const {nombre, empresa, email, telefono, id} = cliente
+
   return (
       <tr className="border-b">
         <td className='p-6 space-y-2'>
@@ -18,6 +19,7 @@ function Cliente({cliente}) {
           <button
             type="button"
             className="text-blue-600 hover:text-blue-800 uppercase font-bold text-xs"
+            onClick={() => navigate(`/clientes/${id}/editar`)}
             >
               Editar
           </button>
---
+++ src/pages/EditarCliente.jsx
@@ -0,0 +1,14 @@
+export async function loader({params}){
+    console.log(params);
+    return null;
+}
+
+const EditarCliente = () => {
+  return (
+    <div>
+      EDITANDO CLIENTE
+    </div>
+  )
+}
+
+export default EditarCliente
