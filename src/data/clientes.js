64834b38d01f1eb2942e1229f3e94fde4072c29f
agustintrossero <agustintrossero@gmail.com>
2023-03-06 16:31:18 +0100

    creando el mensaje de error con status 404 que  evalua si existe el id de cliente usado

--- src/data/clientes.js
+++ src/data/clientes.js
@@ -6,6 +6,15 @@ export async function obtenerClientes(){
     return resultado
 }
 
+export async function obtenerCliente(id){
+    const respuesta = await fetch(`${import.meta.env.VITE_API_URL}/${id}`/*, {
+        method: 'PUT',
+        body: JSON.stringify
+    }*/)
+    const resultado = await respuesta.json()
+    return resultado
+}
+
 export async function agregarCliente(datos){
     try {
         const respuesta = await fetch(import.meta.env.VITE_API_URL, {
--- src/pages/EditarCliente.jsx
+++ src/pages/EditarCliente.jsx
@@ -1,6 +1,15 @@
+import { obtenerCliente } from "../data/clientes";
+
 export async function loader({params}){
-    console.log(params);
-    return null;
+    //console.log(params);
+    const cliente = await obtenerCliente(params.clienteId);
+    if(Object.values(cliente).length === 0) {
+        throw new Response ("", {
+            status: 404,
+            statusText: "El cliente no fue encontrado"
+        })
+    }
+    return cliente
 }
 
 const EditarCliente = () => {
